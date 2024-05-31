09ff27a7092b04446f8454236d1c2ab7eb7f1667
Yuliia1991 <155849406+Yuliia1991@users.noreply.github.com>
2024-05-31 19:22:47 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,12 @@
 function makeArray(firstArray, secondArray, maxLength){
    const newArray = firstArray.concat(secondArray);
-   let element;
-   let quantity;
-   for(element of newArray){
-   if( quantity > maxLength) {
-   
+   if (newArray.length > maxLength) {
+      return newArray.slice([0], maxLength);
+   } else {
+      return newArray;
    }
-   }  return newArray.slice([0], [maxLength]);
-   
-}return newArray;
+}
+
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
